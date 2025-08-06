6c39a9aafa54b05c84fede984f76024137bdbe2e
pedrocaua10 <pedro.caua4@gmail.com>
2025-08-06 13:51:02 -0300

    #

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ComponentMoneyComponent } from './components/component-money/component-money.component';
+import { ComponentModalComponent } from './components/component-modal/component-modal.component';
+import { ComponentModalConfirmComponent } from './components/component-modal-confirm/component-modal-confirm.component';
+import { ComponentModalSuccessComponent } from './components/component-modal-success/component-modal-success.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/money', pathMatch: 'full' },
+  { path: 'money', component: ComponentMoneyComponent },
+  { path: 'cartao', component: ComponentModalComponent },
+  { path: 'confirmar', component: ComponentModalConfirmComponent },
+  { path: 'sucesso', component: ComponentModalSuccessComponent },
+  { path: '**', redirectTo: '/money' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/component-modal-confirm/component-modal-confirm.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TransactionService } from '../../services/transaction.service';
+
+@Component({
+  selector: 'app-component-modal-confirm',
+  templateUrl: './component-modal-confirm.component.html',
+  styleUrls: ['./component-modal-confirm.component.css']
+})
+export class ComponentModalConfirmComponent implements OnInit {
+  dadosTransacao: any = {};
+  destinatario: string = ''; 
+
+  constructor(
+    private router: Router,
+    private transactionService: TransactionService
+  ) {}
+
+  ngOnInit() {
+    this.transactionService.currentData.subscribe(data => {
+      this.dadosTransacao = data || {};
+      this.destinatario = data?.destinatario || ''; 
+    });
+  }
+
+  voltar() {
+    this.router.navigate(['/cartao']);
+  }
+
+  confirmar() {
+    this.router.navigate(['/sucesso']);
+  }
+}
